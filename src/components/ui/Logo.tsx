21dba8d94b24591f100c2090c52975d5644570ee
Patrice <patrice.tano.k@gmail.com>
2026-05-16 23:34:33 +0000

    feat: use AGC Bergerie SVG logo in app header and sidebar

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -5,8 +5,8 @@ interface LogoProps {
 export function Logo({ className = "h-8 w-auto" }: LogoProps) {
   return (
     <img
-      src="/images/LOGO_VH.png"
-      alt="Vases d'Honneur Assemblée Grâce Confondante"
+      src="/logo-agc-bergerie.svg"
+      alt="AGC Bergerie"
       className={className}
     />
   );
